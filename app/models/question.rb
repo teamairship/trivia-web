585b23a8d0ae43e95991909706e3d6ed66649995
Kevin Daniel <kevin.dapps@gmail.com>
2023-05-19 15:59:52 -0400

    Fix ActiveRecord model

--- app/models/question.rb
+++ app/models/question.rb
@@ -18,13 +18,21 @@
 #  index_questions_on_type        (type)
 #
 class Question < ApplicationRecord
+  self.inheritance_column = nil
+
   has_many :answers, class_name: 'Question::Answer', dependent: :destroy
   has_many :game_questions, inverse_of: :question, dependent: :destroy
   has_many :games, through: :game_questions
+
+  accepts_nested_attributes_for :answers
 
   enum difficulty: {
     easy: 0,
     medium: 10,
     hard: 20
+  }
+
+  enum type: {
+    multiple_choice: 0,
   }
 end
